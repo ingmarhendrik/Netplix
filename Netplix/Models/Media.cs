177c9fc5ddd6982e5359abef27204f73ec1197a1
Opilane TTHK <opilane@tthk.ee>
2024-04-10 09:59:51 +0300

    Update media.cs

--- Netplix/Models/Media.cs
+++ Netplix/Models/Media.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace Models
 {
-    internal class Media
+    public class Media
     {
+        public int Id { get; set; }
+        public string DisplayTitle { get; set; }
+        public string MediaType { get; set; } // "movie" or "tv"
+
+        public string Thumbnail { get; set; }
+        public string ThumbnailSmall { get; set; }
+        public string ThumbnailUrl { get; set; }
+
+        public string Overview { get; set; }
+        public string ReleaseDate { get; set; }
+
+        
     }
 }

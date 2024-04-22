85f22745fdd572c99fc8789ac3e396aa78b37573
Opilane TTHK <opilane@tthk.ee>
2024-04-22 10:35:40 +0300

    CategoriesViewModel and Page

---
+++ Netplix/Pages/CategoriesPage.xaml.cs
@@ -0,0 +1,21 @@
+using Netplix.ViewModels;
+
+namespace Netplix.Pages;
+
+public partial class MainPage : ContentPage
+{
+	private readonly HomeViewModel _homeViewModel;
+	public MainPage(HomeViewModel homeViewModel)
+	{
+		InitializeComponent();
+		_homeViewModel = homeViewModel;
+		BindingContext = _homeViewModel;
+	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+		await _homeViewModel.InitializeAsync();
+    }
+
+}
